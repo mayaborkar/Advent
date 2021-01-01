d4236c07fbe1d187c4a99f03c54d18552d81c5fc
mayaborkar <maya.borkar@gmail.com>
2021-01-01 10:04:12 -0800

    Rename files

--- 2020/advent03/advent3.py
+++ 2020/advent03/advent3.py
@@ -1,28 +1,54 @@
-f1 = open("adventlist4a.txt", "r")
+f1 = open("adventlist3ab.txt", "r")
 mylist = f1.readlines()
-totalcount = 0
-validcount = 0
-invalidcount = 0
+# print (len(mylist))
+# print (len(mylist[0]))
+
 for i in range(0, len(mylist)):
-    startrange = int(mylist[i][:mylist[i].find("-")])
-    endrange = int(mylist[i][mylist[i].find("-")+1:mylist[i].find(" ")])
-    letter = mylist[i][mylist[i].find(" ")+1:mylist[i].find(":")]
-    pwd = mylist[i][mylist[i].find(":")+2:]
-    lettercount = int(pwd.count(letter))
-    print(mylist[i])
-    print(startrange)
-    print(endrange)
-    print(letter)
-    print(pwd)
-    print(lettercount)
-    totalcount = totalcount + 1
-#    if startrange <= lettercount and lettercount <= endrange:
-    if lettercount >= startrange and lettercount <= endrange:
-        validcount = validcount + 1
-        print("password is valid")
-    else:
-        invalidcount = invalidcount + 1
-        print("pwd is not valid")
-print(validcount)
-print(invalidcount)
-print(totalcount)
+    mylist[i] = mylist[i][:len(mylist[i])-1]
+    for j in range(0, 7):
+        mylist[i] = mylist[i] + mylist[i]
+
+print ("hello i am here")
+# print (mylist)
+print (len(mylist))
+print (len(mylist[0]))
+
+counter11 = 0
+for i in range(0, len(mylist)):
+    if mylist[i][i] == "#":
+        counter11 = counter11 + 1
+print ("I am here 11 " + str(counter11))
+
+counter13 = 0
+for i in range(0, len(mylist)):
+    if mylist[i][(i*3)] == "#":
+        counter13 = counter13 + 1
+print ("I am here 13 " + str(counter13))
+
+counter15 = 0
+for i in range(0, len(mylist)):
+    if mylist[i][(i*5)] == "#":
+        counter15 = counter15 + 1
+print ("I am here 15 " + str(counter15))
+
+counter17 = 0
+for i in range(0, len(mylist)):
+    if mylist[i][(i*7)] == "#":
+        counter17 = counter17 + 1
+print ("I am here 17 "+ str(counter17))
+
+counter21 = 0
+for i in range(0, int(len(mylist)/2)+1):
+    if mylist[i*2][i] == "#":
+        counter21 = counter21 + 1
+print ("I am here 21 "+ str(counter21))
+
+print counter11
+print counter13
+print counter15
+print counter17
+print counter21
+
+print ("multiple "+str(counter11 * counter13 * counter15 * counter17 * counter21))
+# 2850751488
+# 3207095424

47a9d2b85d55b163f8950bc8d6c1107cad3cce35
mayaborkar <66751353+mayaborkar@users.noreply.github.com>
2021-12-19 17:59:28 -0800

    added comments to files

--- 2021/advent4a.py
+++ 2021/advent4a.py
@@ -1,17 +1,30 @@
 # https://adventofcode.com/2021/day/4
 def printBoard(board):
-    newBoard = []
-    for i in range(0, len(board)):
-        if(i % 5 != 0):
-            newBoard[i] = board[i]
-        print(board[i])
-
+    for x in board:
+        print("Printing next board... ")
+        for y in x:
+            print(y)
+# take out null arrays (RESOLVED)
+# if array starts with 1 digit number delete the null (RESOLVED)
+# not printing last board (RESOLVED)
 
 f1 = open("adventinput4.txt", "r")
 mylist = f1.readlines()
-drawnNumbers = mylist[0].split("\n")
+drawnNumbers = mylist[0].replace("\n", "").split(",")
 
-board1 = "14 33 79 61 44 85 60 38 13 48 51 34 11 19  7 21 30 73  6 76 41  4 65 18 91"
-printBoard(board1)
-print(board1)
+tempBoard = []
+newGame = []
+for i in range(2, len(mylist)):
+
+    tempBoard.append(mylist[i].replace("\n", "").replace("  ", " ").split(" "))
+    if len(tempBoard[len(tempBoard) - 1][0]) == 0:
+        tempBoard[len(tempBoard) - 1].pop(0)
+
+    if len(tempBoard) == 6:
+        tempBoard.pop()
+        newGame.append(tempBoard)
+        tempBoard = []
+
+printBoard(newGame)
+
 

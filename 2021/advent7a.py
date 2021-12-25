8dfecca192c74ba739af791d9a33c5b75a6e873f
mayaborkar <66751353+mayaborkar@users.noreply.github.com>
2021-12-24 19:04:23 -0800

    advent day 5 update

--- 2021/advent7a.py
+++ 2021/advent7a.py
@@ -1,9 +1,35 @@
 f1 = open("adventinput7.txt", "r")
 mylist = f1.read()
-for i in range(0, len(mylist)):
-    mylist[i].split(',')
-print(mylist)
 
+crabList = mylist.split(',')
 
+# using for loop for each crab pos
+print(crabList)
+crabDistance = []
+for i in range(0, len(crabList)):
+    totalFuel = 0
+    for j in range(0, len(crabList)):
+        totalFuel += abs(int(crabList[i]) - int(crabList[j]))
+    crabDistance.append([crabList[i], totalFuel])
 
+index = 0
+leastFuel = crabDistance[0][1]
+for i in range(0, len(crabDistance)):
+    if crabDistance[i][1] < leastFuel:
+        index = i
+        leastFuel = crabDistance[i][1]
+print(crabDistance[index])
 
+'''
+# using average
+sum = 0
+for i in range(0, len(crabList)):
+    sum += int(crabList[i])
+average = sum/(len(crabList))
+
+# fuel amount
+fuelAmount = 0
+for i in range(0, len(crabList)):
+    fuelAmount += abs(int(crabList[i]) - average)
+print(fuelAmount)
+'''

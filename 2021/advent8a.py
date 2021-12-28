7aa970152c19f158d99c099197501bcc86c549d0
mayaborkar <66751353+mayaborkar@users.noreply.github.com>
2021-12-27 18:05:58 -0800

    advent day 7 update

--- 2021/advent8a.py
+++ 2021/advent8a.py
@@ -1,6 +1,26 @@
-f1 = open("adventinput8.txt", "r")
-mylist = f1.read()
+f1 = "adventinput8.txt"
+# mylist = f1.read()
+digitsOutput =[]
 
+for x in open(f1):
+    digits1, digits2 = x.strip().replace('\n','').split('|')
+    digitsOutput.append(digits2.strip().split(' '))
+print(digitsOutput)
 
-digits = mylist.replace('\n', '').split('|')
-print(digits)
+oneCount = 0
+fourCount = 0
+sevenCount = 0
+eightCount = 0
+
+for i in range(0, len(digitsOutput)):
+    for j in range(0, len(digitsOutput[i])):
+        if len(digitsOutput[i][j]) == 2:
+            oneCount += 1
+        if len(digitsOutput[i][j]) == 4:
+            fourCount += 1
+        if len(digitsOutput[i][j]) == 3:
+            sevenCount += 1
+        if len(digitsOutput[i][j]) == 7:
+            eightCount += 1
+
+print(oneCount + fourCount + sevenCount + eightCount)

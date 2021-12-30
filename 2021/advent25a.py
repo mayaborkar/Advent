78251943674f8cf89e235955bff17047a31639a6
mayaborkar <66751353+mayaborkar@users.noreply.github.com>
2021-12-30 14:57:57 -0800

    advent day 7 update

--- 2021/advent25a.py
+++ 2021/advent25a.py
@@ -1,2 +1,21 @@
-f1 = open("adventtestinput25.txt", "r")
-mylist = f1.read()
+
+f1 = open("adventinput25.txt", "r")
+mylist = f1.readlines()
+moveArray = []
+moves = []
+for i in range(0, len(mylist)):
+    moves = mylist[i].strip().split('\n')
+    moveArray.append(moves)
+
+stepCount = 0
+
+validmove = True
+for i in range(0, len(moveArray)):
+    if moveArray[i] == ">" and moveArray[i + 1] != ".":
+        validmove = False
+
+return validmove
+
+
+print stepCount
+

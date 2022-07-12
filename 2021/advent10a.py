e83f995ba00f666b1c27f4a61bb09991e75f26ee
mayaborkar <66751353+mayaborkar@users.noreply.github.com>
2022-07-12 09:56:07 -0700

    advent day 10 V1

--- 2021/advent10a.py
+++ 2021/advent10a.py
@@ -1,3 +1,35 @@
-f1 = open("adventinput10.txt", "r")
+def correspondingDel(delimeter):
+    if delimeter == "<":
+        return ">"
+    if delimeter == "{":
+        return "}"
+    if delimeter == "[":
+        return "]"
+    if delimeter == "(":
+        return ")"
+    return "not valid"
+
+
+f1 = open("adventtestinput10.txt", "r")
 mylist = f1.read().split('\n')
 print (mylist)
+openDels = ["<", "{", "[", "("]
+valid = []
+points = 0
+for i in range(0, len(mylist) - 1):
+    if mylist[i] in openDels:
+        valid.append(mylist[i])
+    if mylist[i] not in openDels:
+        if mylist[i+1] == correspondingDel(mylist[i]):
+            valid.pop()
+        else:
+            if valid[len(valid)] == "<":
+                points += 25137
+            if valid[len(valid)] == "{":
+                points += 1197
+            if valid[len(valid)] == "[":
+                points += 57
+            if valid[len(valid)] == "(":
+                points += 3
+print(points)
+

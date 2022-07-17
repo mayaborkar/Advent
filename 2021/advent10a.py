b1c8db7c37838482cfd0f036f3d33d2b5d3534f3
mayaborkar <66751353+mayaborkar@users.noreply.github.com>
2022-07-16 17:10:34 -0700

    advent day 10 V1

--- 2021/advent10a.py
+++ 2021/advent10a.py
@@ -10,26 +10,42 @@ def correspondingDel(delimeter):
     return "not valid"
 
 
-f1 = open("adventtestinput10.txt", "r")
+f1 = open("adventinput10.txt", "r")
 mylist = f1.read().split('\n')
-print (mylist)
+# print (mylist)
 openDels = ["<", "{", "[", "("]
 valid = []
 points = 0
-for i in range(0, len(mylist) - 1):
-    if mylist[i] in openDels:
-        valid.append(mylist[i])
-    if mylist[i] not in openDels:
-        if mylist[i+1] == correspondingDel(mylist[i]):
-            valid.pop()
+for i in range(0, len(mylist)):
+    # print("current string: " + mylist[i])
+    for j in range(0, len(mylist[i]) - 1):
+        # print("current character: " + mylist[i][j])
+        if mylist[i][j] in openDels:
+            # print("current open character: " + mylist[i][j])
+            valid.append(mylist[i][j])
         else:
-            if valid[len(valid)] == "<":
-                points += 25137
-            if valid[len(valid)] == "{":
-                points += 1197
-            if valid[len(valid)] == "[":
-                points += 57
-            if valid[len(valid)] == "(":
-                points += 3
+            # print("current closed character: " + mylist[i][j])
+            if mylist[i][j] == correspondingDel(valid[-1]):
+                # print("found corresponding closed, now popping " + str(valid))
+                valid.pop()
+                # print("after popping: " + str(valid))
+            else:
+                # print("adding points\n")
+                # print("wrong closed character: " + mylist[i][j])
+                # print("previous characters: " + mylist[i][0:j])
+                if mylist[i][j] == ">":
+                    # print("adding 25137")
+                    points += 25137
+                if mylist[i][j] == "}":
+                    # print("adding 1197")
+                    points += 1197
+                if mylist[i][j] == "]":
+                    # print("adding 57")
+                    points += 57
+                if mylist[i][j] == ")":
+                    # print("adding 3")
+                    points += 3
+                # print("printing partial points " + str(points))
+                break
 print(points)
 

73c0f53432a7832c19d3746a4cf0d6cd0c5bd1f2
mayaborkar <66751353+mayaborkar@users.noreply.github.com>
2022-12-04 13:27:42 -0800

    Advent 2022

---
+++ 2022/advent20222b.py
@@ -0,0 +1,43 @@
+def isWin(opponent):
+    if opponent == "A":
+        return "Y"
+    if opponent == "B":
+        return "Z"
+    if opponent == "C":
+        return "X"
+
+
+def isDraw(opponent):
+    if opponent == "A":
+        return "X"
+    if opponent == "B":
+        return "Y"
+    if opponent == "C":
+        return "Z"
+
+
+f1 = open("2022adventinput2", "r")
+mylist = f1.read().replace(' ', "").split("\n")
+
+# auto-points 1-Rock, 2-Paper, 3-Scissors
+# winning points 0-Lose, 3-Draw, 6-Win
+# opponent: A-Rock, B-Paper, C-Scissors
+# me: X-Rock, Y-Paper, Z-Scissors
+# add to get the total
+# X - lose, Y- draw, Z - win
+
+sum = 0
+print(mylist)
+for i in range(0, len(mylist)):
+    if isWin(mylist[i][0]) == mylist[i][1]:
+        sum += 6
+    elif isDraw(mylist[i][0]) == mylist[i][1]:
+        sum += 3
+    # lose sum += 0
+    if mylist[i][1] == "X":
+        sum += 1
+    elif mylist[i][1] == "Y":
+        sum += 2
+    elif mylist[i][1] == "Z":
+        sum += 3
+print(sum)

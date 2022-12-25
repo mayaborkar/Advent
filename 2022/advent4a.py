c9fedf775cdd7e4a0679292d04e9c3b14f48a8be
mayaborkar <66751353+mayaborkar@users.noreply.github.com>
2022-12-25 13:40:30 -0800

    Unversionsed files.

--- 2022/advent4a.py
+++ 2022/advent4a.py
@@ -1,6 +1,7 @@
 f1 = open("adventinput4", "r")
-mylist = f1.read().split("\n\n")
+mylist = f1.read().split("\n")
 
 # find all the numbers in the first range
 # compare it with the numbers in the second range
 # if all the numbers are in both ranges --> +1
+print(mylist)

c1ad729f39d96c491d1aa65ac813650d7e0f6f21
mayaborkar <66751353+mayaborkar@users.noreply.github.com>
2022-12-26 07:07:58 -0800

    Unversionsed files.

--- 2022/advent4a.py
+++ 2022/advent4a.py
@@ -1,7 +1,19 @@
-f1 = open("adventinput4", "r")
-mylist = f1.read().split("\n")
+mylist = open("adventinput4", "r").read().splitlines()
 
-# find all the numbers in the first range
-# compare it with the numbers in the second range
-# if all the numbers are in both ranges --> +1
-print(mylist)
+
+t = 0
+
+
+for i in mylist:
+
+    front, back = i.split(",")
+
+    frontfront, frontfrontfront = [int(r) for r in front.split("-")]
+    backback, backbackback = [int(r) for r in back.split("-")]
+
+
+    if (frontfront <= backback and frontfrontfront >= backbackback ) or (backback<=frontfront and backbackback>=frontfrontfront):
+        t+=1
+
+
+print(t)

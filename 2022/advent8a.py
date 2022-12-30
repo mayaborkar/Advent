390bbd464395b83725a9e93023d348d753ad3286
mayaborkar <66751353+mayaborkar@users.noreply.github.com>
2022-12-29 19:00:45 -0800

    Unversionsed files.

--- 2022/advent8a.py
+++ 2022/advent8a.py
@@ -1,20 +1,37 @@
 f1 = open("adventtestinput8", "r")
 myinputlist = f1.readlines()
 # look up, down, left, and right for each of the trees
-
-mylist = [[]]
+print(myinputlist)
+mylist = []
+treeGrid = []
 count = 0
-for i in range(1, len(myinputlist) - 1):
-    count += 2
-    mylist.append(myinputlist[i].replace("\n", ""))
+for i in range(0, len(myinputlist)):
+    # count += 2
+    mylist = [*myinputlist[i]]
+    mylist.pop()
+    treeGrid.append(mylist)
+    # print(mylist)
+print(treeGrid)
+for row in range(1, len(treeGrid) - 1):
+    for col in range(1, len(treeGrid[row]) - 1):
+        currentTree = treeGrid[row][col]
+        top = treeGrid[row - 1][col]
+        bottom = treeGrid[row + 1][col]
+        left = treeGrid[row][col - 1]
+        right = treeGrid[row][col + 1]
+        if((currentTree > top) or (currentTree > bottom) or (currentTree > left) or (currentTree > right)):
+            count += 1
+print(count)
 
-print(mylist)
+'''
+    for j in range(1, len(mylist[i]) - 1):
+        if (mylist[i][j] > mylist[i][j + 1]):
+            if (mylist[i][j] > mylist[i][j]):
+                if (mylist[i][j] > mylist[i + 1][j]):
+                    if (mylist[i][j] > mylist[i - 1][j]):
+                        count += 1
+
 count += 2 * len(mylist[0])
 print(count)
 '''
-if (mylist[i][j] > mylist[i][j + 1]):
-    if (mylist[i][j] > mylist[i][j - 1]):
-        if (mylist[i][j] > mylist[i + 1][j]):
-            if (mylist[i][j] > mylist[i - 1][j]):
-                count += 1
-'''
+

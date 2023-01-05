4cc92da0aa153040f9e1041abc427addca8da077
mayaborkar <66751353+mayaborkar@users.noreply.github.com>
2023-01-05 10:20:51 -0800

    working on 25

--- 2022/advent25.py
+++ 2022/advent25.py
@@ -1,10 +1,36 @@
+def convertToSnafu(decimal):
+    final = ""
+    current = decimal
+    while(current > 0):
+        final += str(current % 5)
+        current = decimal//5
+
+    print(final)
+
+
 f1 = open("adventtestinput25", "r")
 inputlist = f1.readlines()
 mylist = []
-for i in range(0, len(inputlist)):
-    mylist.append(inputlist[i].replace("=", "-2").replace("-", "-1").split())
+current = ""
 total = 0
-
-
-
-print(mylist)
+for i in range(0, len(inputlist)):
+    number = 0
+    currNum = 0
+    current = inputlist[i].replace("\n", "")
+    current = current[::-1]
+    # print(current, current[::-1])
+    for j in range(0, len(current)):
+        if(current[j] == "-"):
+            currNum = -1
+        elif(current[j] == "="):
+            currNum = -2
+        else:
+            currNum = int(current[j])
+        number += (5**j) * currNum
+    total += number
+    print(current, number)
+    # print(inputlist[i].replace("\n", ""))
+print("converted" + str(convertToSnafu(31)))
+# convert back to SNAFU
+print(total)
+print(inputlist)

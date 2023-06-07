148818e92593331a50c288a3a576cb19caec6269
mayaborkar <maya.borkar@gmail.com>
2023-06-07 13:04:11 -0700

    fixing convertToSnafu

--- 2022/advent25a.py
+++ 2022/advent25a.py
@@ -19,7 +19,8 @@ for snafu in snafulist:
     # print (snafu,snafu[j],j,i, fuel, fueltotal)
     fueltotal += fuel
 
-print(fueltotal)fivex = [5 ** x for x in range(0, int(fueltotal / (183488001053))) if 5 ** x < fueltotal]
+print(fueltotal)
+fivex = [5 ** x for x in range(0, int(fueltotal / (183488001053))) if 5 ** x < fueltotal]
 print(fivex[::-1])
 sanfuend = []
 sanfuend1 = []

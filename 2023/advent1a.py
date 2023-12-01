451f0ed628454986699b015e7bb3af8b60e58bcf
mayaborkar <maya.borkar@gmail.com>
2023-12-01 13:05:31 -0800

    fixing convertToSnafu

---
+++ 2023/advent1a.py
@@ -0,0 +1,25 @@
+f1 = open("adventinput1", "r")
+mylist = f1.read().split('\n')
+
+digits = []
+currentDigits = []
+for i in range(0, len(mylist)):
+    currentDigits = []
+    for j in range(0, len(mylist[i])):
+        if mylist[i][j].isdigit():
+            currentDigits.append(mylist[i][j])
+    if (len(currentDigits) > 0):
+        digits.append(currentDigits[0])
+        digits.append(currentDigits[len(currentDigits) - 1])
+
+    print(mylist[i])
+
+total = 0
+for k in range(0, len(digits)):
+    if k % 2 == 0:
+        total += 10 * int(digits[k])
+    else:
+        total += int(digits[k])
+
+print(total)
+print(digits)

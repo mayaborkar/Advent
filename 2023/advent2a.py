644449ec2f7e7dbe7cf968d5c88b79b34ce16b33
mayaborkar <maya.borkar@gmail.com>
2023-12-03 10:46:15 -0800

    advent 2023

--- 2023/advent2a.py
+++ 2023/advent2a.py
@@ -1,8 +1,46 @@
-f1 = open("adventinput1", "r")
-mylist = f1.read().split('\n')
+f1 = open("advent2input.txt", "r")
+inputlist = f1.readlines()
 
-sets = []
-gameid = []
-for i in range(0, len(mylist)):
-    sets.append(mylist[i].split(';'))
-    gameid.append(mylist[i].split(':'))
+gameId = [c.replace('\n', '').split(': ') for c in inputlist]
+
+total = 0
+red = 12
+green = 13
+blue = 14
+
+for i in range (0,len(gameId)):
+    redCount=0
+    greenCount=0
+    blueCount=0
+    possible = True
+
+    gameId[i][0] = int(gameId[i][0].replace('Game ', ''))
+    gamearr1 = gameId[i][1].split(';')
+    gamearr2 = [c.split(',') for c in gamearr1]
+
+    for x in gamearr2:
+        for c in x:
+            if c.__contains__('blue'):
+                # bx += int(c.replace("blue",""))
+                if int(c.replace("blue","")) > blue:
+                    possible=False
+            elif c.__contains__('red'):
+                # rx += int(c.replace("red",""))
+                if int(c.replace("red","")) > red:
+                    possible=False
+            elif c.__contains__('green'):
+                # gx += int(c.replace("green",""))
+                if int(c.replace("green","")) > green:
+                    possible=False
+            if not possible:
+                break
+        if not possible:
+            break
+    if possible:
+        total += gameId[i][0]
+        print(gameId[i][0], gamearr2)
+        print("adding to total ", total)
+    else:
+        print('Not possible ', gameId[i][0], gamearr2)
+
+print(total)
---
+++ 2023/advent3a.py
@@ -0,0 +1,6 @@
+f1 = open("adventinput3", "r")
+inputlist = f1.readlines()
+
+for i in range(0, len(inputlist)):
+    # check if it is next to/diagonal to a viable symbol
+    # add all of the viable numbers *10^3 the place that they are in

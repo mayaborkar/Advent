305e27be96708cb78b8476d989e9894e6553e6ab
mayaborkar <maya.borkar@gmail.com>
2023-12-05 17:30:24 -0800

    advent 2023

--- 2023/advent4a.py
+++ 2023/advent4a.py
@@ -1,33 +1,34 @@
-
 f1 = open("adventinput4", "r")
 inputlist = f1.readlines()
 
 cardArr = []
 numbers = []
 
-winning = []
-yourNums = []
 count = 0
 total = 0
 
+# print (inputlist)
+
 for i in range(0, len(inputlist)):
+    count = 0
     cardArr.append(inputlist[i].replace("  ", " ").replace("\n", "").split(":"))
-    numbers.append(cardArr[i][1].split("|"))
-    print(numbers)
-    for j in range(0, len(numbers)):
-        winning = (numbers[0][j].split(" "))
-        # print(winning)
-        yourNums = (numbers[j][1].split(" "))
+    # print (cardArr)
+    numbers = (cardArr[i][1].split(" | "))
+    numbers1 = numbers[0].split(' ')
+    numbers2 = numbers[1].split(' ')
+    # print (numbers1)
+    for k in numbers1:
+        if (k == ''):
+            continue
+        if(k in numbers2):
+            count += 1
+            # print('match ', k, numbers)
+    if (count > 0):
+        total += 2**(count-1)
 
-for k in range(0, len(yourNums)):
-    if (yourNums == ''):
-        continue
-    if(yourNums[k] in winning):
-        print(yourNums[k])
-        count += 1
 
-print(winning)
-print(yourNums)
-print(count)
-total = 2**(count-2)
+# print(count)
 print(total)
+
+
+#23384 --> too high

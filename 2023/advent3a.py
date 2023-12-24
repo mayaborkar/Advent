81fb8c033922e03c7052606f6c78b23b496b7db5
mayaborkar <maya.borkar@gmail.com>
2023-12-24 09:35:54 -0800

    advent 8 2023

--- 2023/advent3a.py
+++ 2023/advent3a.py
@@ -1,6 +1,19 @@
+def symbolArray(input):
+    specialIndex = []
+    notAllowed = "0123456789."
+    for i in range(0, len(input)):
+        for j in range(0, len(input[i])):
+            if(input[i][j] not in notAllowed):
+                specialIndex.append([i,j])
+
+
 f1 = open("adventinput3", "r")
 inputlist = f1.readlines()
 
+# check if it is next to/diagonal to a viable symbol
+# add all of the viable numbers *10^# the place that they are in
+
 for i in range(0, len(inputlist)):
-    # check if it is next to/diagonal to a viable symbol
-    # add all of the viable numbers *10^# the place that they are in
+    for j in range(0, len(inputlist[i])):
+
+

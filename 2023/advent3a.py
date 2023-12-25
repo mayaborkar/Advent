bbd25a06624b71df177d387b067c631d15bd664b
mayaborkar <maya.borkar@gmail.com>
2023-12-24 19:18:14 -0800

    advent 3 2023

--- 2023/advent3a.py
+++ 2023/advent3a.py
@@ -1,11 +1,13 @@
 def symbolArray(input):
     specialIndex = []
     notAllowed = "0123456789."
+
     for i in range(0, len(input)):
         for j in range(0, len(input[i])):
             # print(input[i][j])
             if(input[i][j] not in notAllowed):
                 specialIndex.append([i,j])
+
     return specialIndex
 
 
@@ -17,50 +19,48 @@ def adjacent(input):
 
     for c in special:
         if (input[c[0] - 1][ c[1] - 1] in '0123456789'):
-            surroundingNums.append(input[c[0]-1][c[1] - 1])
-        if (input[c[0]][ c[1] - 1] in '0123456789'):
-            surroundingNums.append(input[c[0]][c[1] - 1])
+            surroundingNums.append(wholeNum(input[c[0]], c[1], "D"))
+            # surroundingNums.append(input[c[0]-1][c[1] - 1])
+        if (input[c[0]][c[1] - 1] in '0123456789'):
+            surroundingNums.append(wholeNum(input[c[0]], c[1], "L"))
+            # surroundingNums.append(input[c[0]][c[1] - 1])
         if (input[c[0] + 1][ c[1] - 1] in '0123456789'):
-            surroundingNums.append(input[c[0] + 1][c[1] - 1])
+            surroundingNums.append(wholeNum(input[c[0]], c[1], "D"))
+            # surroundingNums.append(input[c[0] + 1][c[1] - 1])
         if (input[c[0] - 1][ c[1]] in '0123456789'):
-            surroundingNums.append(input[c[0] - 1][c[1]])
+            surroundingNums.append(wholeNum(input[c[0]], c[1], "D"))
+            # surroundingNums.append(input[c[0] - 1][c[1]])
         if (input[c[0] + 1][ c[1]] in '0123456789'):
-            surroundingNums.append(input[c[0] + 1][c[1]])
+            surroundingNums.append(wholeNum(input[c[0]], c[1], "D"))
+            # surroundingNums.append(input[c[0] + 1][c[1]])
         if (input[c[0] - 1][ c[1] + 1] in '0123456789'):
-            surroundingNums.append(input[c[0] - 1][c[1] + 1])
+            surroundingNums.append(wholeNum(input[c[0]], c[1], "D"))
+           # surroundingNums.append(input[c[0] - 1][c[1] + 1])
         if (input[c[0]][ c[1] + 1] in '0123456789'):
-            surroundingNums.append(input[c[0]][c[1] + 1])
+            surroundingNums.append(wholeNum(input[c[0]], c[1], "R"))
+            # surroundingNums.append(input[c[0]][c[1] + 1])
         if (input[c[0] + 1][ c[1] + 1] in '0123456789'):
-            surroundingNums.append(input[c[0] + 1][c[1] + 1])
+            surroundingNums.append(wholeNum(input[c[0]], c[1], "D"))
+            # surroundingNums.append(input[c[0] + 1][c[1] + 1])
 
 
         print(c[0], c[1])
-    '''
-    for i in range(0, len(input)):
-        for j in range(0, len(input[i])):
-            try:
-                print("here ", input[i][j], i, j)
-                if(([i - 1, j - 1] or
-                [i, j - 1] or
-                [i + 1, j - 1] or
-                [i - 1, j] or
-                [i + 1,j] or
-                [i - 1, j + 1] or
-                [i, j + 1] or
-                [i + 1, j + 1]) in special):
-                    print("inside")
-                    print(special)
-                    if input[i][j] in '0123456789' :
-                        surroundingNums.append(input[i][j])
-                        print (surroundingNums)
-            except:
-                print("error ",NameError)
-                continue
-    '''
     return('Returning surrounding ', surroundingNums)
 
 
 
+def wholeNum(line, position, side):
+    print(line[:position])
+    if side.__eq__("L"):
+        index = line[:position].rfind('.')
+        if(index == -1):
+            index = 0
+        return(line[index:position])
+    elif side.__eq__("R"):
+        index = line[position:].find('.')
+        if(index == len(line)):
+            index = len(line)
+        return(line[position:index + 1])
 
 
 
@@ -70,7 +70,8 @@ inputlist = f1.read().split('\n')
 # print(inputlist)
 # check if it is next to/diagonal to a viable symbol
 # add all of the viable numbers *10^# the place that they are in
+print(wholeNum(inputlist[5], 7, "R"))
 
-print(adjacent(inputlist))
+#print(adjacent(inputlist))
 
 

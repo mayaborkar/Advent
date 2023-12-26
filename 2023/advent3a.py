520642dc7d849a5e2f8927902fa8215c789e330f
mayaborkar <maya.borkar@gmail.com>
2023-12-26 08:35:13 -0800

    advent 3 2023

--- 2023/advent3a.py
+++ 2023/advent3a.py
@@ -20,28 +20,36 @@ def adjacent(input):
 
     for c in special:
         if (input[c[0] - 1][ c[1] - 1] in '0123456789'):
-            surroundingNums.append(wholeNum(input[c[0]], c[1], "D"))
+            surroundingNums.append(wholeNum(input[c[0]], c[1], "D", startEnd)[0])
+            startEnd.append(wholeNum(input[c[0]], c[1], "D", startEnd)[1])
             # surroundingNums.append(input[c[0]-1][c[1] - 1])
         if (input[c[0]][c[1] - 1] in '0123456789'):
-            surroundingNums.append(wholeNum(input[c[0]], c[1], "L"))
+            surroundingNums.append(wholeNum(input[c[0]], c[1], "L", startEnd)[0])
+            startEnd.append(wholeNum(input[c[0]], c[1], "L", startEnd)[1])
             # surroundingNums.append(input[c[0]][c[1] - 1])
         if (input[c[0] + 1][ c[1] - 1] in '0123456789'):
-            surroundingNums.append(wholeNum(input[c[0]], c[1], "D"))
+            surroundingNums.append(wholeNum(input[c[0]], c[1], "D", startEnd)[0])
+            startEnd.append(wholeNum(input[c[0]], c[1], "D", startEnd)[1])
             # surroundingNums.append(input[c[0] + 1][c[1] - 1])
         if (input[c[0] - 1][ c[1]] in '0123456789'):
-            surroundingNums.append(wholeNum(input[c[0]], c[1], "D"))
+            surroundingNums.append(wholeNum(input[c[0]], c[1], "D", startEnd)[0])
+            startEnd.append(wholeNum(input[c[0]], c[1], "D", startEnd)[1])
             # surroundingNums.append(input[c[0] - 1][c[1]])
         if (input[c[0] + 1][ c[1]] in '0123456789'):
-            surroundingNums.append(wholeNum(input[c[0]], c[1], "D"))
+            surroundingNums.append(wholeNum(input[c[0]], c[1], "D", startEnd)[0])
+            startEnd.append(wholeNum(input[c[0]], c[1], "D", startEnd)[1])
             # surroundingNums.append(input[c[0] + 1][c[1]])
         if (input[c[0] - 1][ c[1] + 1] in '0123456789'):
-            surroundingNums.append(wholeNum(input[c[0]], c[1], "D"))
+            surroundingNums.append(wholeNum(input[c[0]], c[1], "D", startEnd[0]))
+            startEnd.append(wholeNum(input[c[0]], c[1], "D", startEnd)[1])
            # surroundingNums.append(input[c[0] - 1][c[1] + 1])
         if (input[c[0]][ c[1] + 1] in '0123456789'):
-            surroundingNums.append(wholeNum(input[c[0]], c[1], "R"))
+            surroundingNums.append(wholeNum(input[c[0]], c[1], "R", startEnd)[0])
+            startEnd.append(wholeNum(input[c[0]], c[1], "R", startEnd)[1])
             # surroundingNums.append(input[c[0]][c[1] + 1])
         if (input[c[0] + 1][ c[1] + 1] in '0123456789'):
-            surroundingNums.append(wholeNum(input[c[0]], c[1], "D"))
+            surroundingNums.append(wholeNum(input[c[0]], c[1], "D", startEnd)[0])
+            startEnd.append(wholeNum(input[c[0]], c[1], "D", startEnd)[1])
             # surroundingNums.append(input[c[0] + 1][c[1] + 1])
 
 
@@ -50,21 +58,23 @@ def adjacent(input):
 
 
 
-def wholeNum(line, position, side):
+def wholeNum(line, position, side, startEndArr):
     print(line[:position])
     if side.__eq__("L"):
         index = line[:position].rfind('.')
         if(index == -1):
             index = 0
-        # startEndArr.append(index, position)
-        return(line[index:position])
+        if [index, position] not in startEndArr:
+            startEndArr.append([index, position])
+            return(line[index:position], startEndArr)
     elif side.__eq__("R"):
         index = line[position:].find('.')
         print(index)
         if(index == -1):
             index = len(line)
-        # startEndArr.append(position + 1, position + index)
-        return(line[position + 1: position + index])
+        if [position + 1, position + index] not in startEndArr:
+            startEndArr.append([position + 1, position + index])
+            return(line[position + 1: position + index], startEndArr)
 
     elif side.__eq__("D"):
         indexl = line[position:].find('.')
@@ -73,8 +83,9 @@ def wholeNum(line, position, side):
             indexl = 0
         if(indexr == -1):
             indexr = len(line)
-        # startEndArr.append(indexl, position + indexr)
-        return(line[indexl:position + indexr])
+        if [indexl, position + indexr] not in startEndArr:
+            startEndArr.append([indexl, position + indexr])
+            return(line[indexl:position + indexr], startEndArr)
 
 
 
@@ -85,6 +96,7 @@ inputlist = f1.read().split('\n')
 # print(inputlist)
 # check if it is next to/diagonal to a viable symbol
 # add all of the viable numbers *10^# the place that they are in
-print(wholeNum(inputlist[5], 6, "R"))
+# print(wholeNum(inputlist[5], 6, "R"))
 
+print(sum(adjacent(inputlist)))
 #print(adjacent(inputlist))

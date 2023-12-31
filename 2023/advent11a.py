0dc37339736f06c8ca25958030f8fac59255cc6c
mayaborkar <maya.borkar@gmail.com>
2023-12-31 11:12:51 -0800

    advent 11 2023

--- 2023/advent11a.py
+++ 2023/advent11a.py
@@ -1,37 +1,51 @@
-def blankCols(column, index):
-
-
+def expand(blank, start, end):
+    count = 0
+    for c in blank:
+        if (start <= c <= end or end <= c <= start):
+            count += 1
+    return count
 
 f1 = open("adventinput11", "r")
 mylist = f1.read().split("\n")
 # print(mylist)
-galaxies = {}
+
 coordinates = []
 xBlankArr = []
-yBlankArr = []
+
 for i in range(0, len(mylist)):
-    if (mylist[i].count(".") == len(mylist[i])):
+    if ("#" not in mylist[i]):
         # print("here")
         xBlankArr.append(i)
     for j in range(0, len(mylist[i])):
-        if(blankCols(mylist[i], mylist[i][j])):
-            yBlankArr.append(j)
         if (mylist[i][j].__eq__("#")):
             coordinates.append((i, j))
-distArr = []
 
+yBlankArr = []
+for i in range(0, len(mylist)):
+    switch = ''
+    for j in range(0, len(mylist[i])):
+        switch += mylist[j][i]
+    if ("#" not in switch):
+        yBlankArr.append(i)
+
+totalDist = 0
+distance = 0
 for c in coordinates:
     distArr = []
-
     for c_other in coordinates:
         #print (c, '=', c_other)
-        if(abs(c[0] - c_other[0]) + abs(c[1] - c_other[1]).__ne__(0)):
-            distArr.append(abs(c[0] - c_other[0]) + abs(c[1] - c_other[1]))
-    galaxies[c] = min(distArr)
+        if(c != c_other ):
+            distance = abs(c[0] - c_other[0]) + abs(c[1] - c_other[1])
+            expandRow = expand(xBlankArr, c[0], c_other[0])
+            expandCol = expand(yBlankArr, c[1], c_other[1])
+            distance = distance + (1000000*expandRow) + (1000000*expandCol) - (expandRow+expandCol)
+            totalDist += distance
 
 print(xBlankArr)
-print("y " + yBlankArr)
-print(galaxies)
+print(yBlankArr)
+print("total distance", totalDist/2)
+
+# 339317600874
 '''
 distance(x1-x2 + y1-y2)
 x,y of the * and the distances of each of the pairs

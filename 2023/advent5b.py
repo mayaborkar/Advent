9e2e2990e34286aa89f20e8bc07d2a1457db1473
mayaborkar <maya.borkar@gmail.com>
2024-01-05 10:55:39 -0800

    advent 5b 2023

--- 2023/advent5b.py
+++ 2023/advent5b.py
@@ -11,11 +11,34 @@ def findLocation(arr, s1):
     return int(s1)
 
 
+def overlap(range1, range2):
+    # given two tuples see if their ranges intersect
+    # should return a tuple
+    start1, end1 = range1
+    start2, end2 = range2
+
+    # Check if ranges intersect
+    if start1 <= end2 and start2 <= end1:
+        # Calculate intersection
+        intersection_start = max(start1, start2)
+        intersection_end = min(end1, end2)
+        return (intersection_start, intersection_end)
+    else:
+        # No intersection
+        return -1
+
+def overlapList(range, mappings):
+
+
+
+
 f1 = open("adventinput5", "r")
 seedList = f1.read().split(" ")
 #print(findLocation(mylist, 79))
 print(seedList)
 location = []
+print(overlap((50, 60), (55, 70)))
+'''
 for i in range(0, len(seedList)):
     location.append(readFiles("htol",
         readFiles("ttoh",
@@ -24,5 +47,5 @@ for i in range(0, len(seedList)):
         readFiles("ftow",
         readFiles("stof",
         readFiles("stos", seedList[i]))))))))
-
-print(min(location))
+'''
+# print(min(location))

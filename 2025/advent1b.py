c6274a0564d89f52da623c97158a7a4602594bc8
Maya Borkar <maya.borkar@gmail.com>
2025-12-01 00:16:04 -0500

    solving advent 1 parts 1 and 2

--- 2025/advent1b.py
+++ 2025/advent1b.py
@@ -1,19 +1,23 @@
 f1 = open("2025/adventinput1", "r")
 
 mylist = [line.strip() for line in f1 if line.strip()]
- 
+
 position = 50
 count = 0
 
 for line in mylist:
     direction = line[0]
     distance = int(line[1:])
-    if direction == 'R':
-        position = (position + distance) % 100
-    else:
-        position = (position - distance) % 100
 
-    if position == 0:
-        count += 1
+    if direction == 'R':
+        step = 1
+    else:
+        step = -1
+
+    for i in range(distance):
+        position = (position + step) % 100
+
+        if position == 0:
+            count += 1
 
 print(count)
